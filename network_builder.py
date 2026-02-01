7387511cc482f4db40e0ac34ee18e73b1f50a2b9
Shumye Ayalneh <shmuye27@gmail.com>
2026-02-01 19:16:27 +0300

    Initialize NetworkBuilder class with imports and basic skeleton

---
+++ network_builder.py
@@ -0,0 +1,8 @@
+import networkx as nx
+from datetime import datetime
+from data_simulator import DataSimulator
+import config
+
+class NetworkBuilder:
+    """Manages network construction and updates"""
+    pass

deb527b3fb4e9617141b8fba09b5d4eacb5ad027
natnael-eyuel-dev <natnael.eyuel.dev@gmail.com>
2026-02-01 19:38:58 +0300

    feat: add MetricsCalculator class scaffold with caches

--- metrics_calculator.py
+++ metrics_calculator.py
@@ -3,3 +3,11 @@ import networkx as nx
 import numpy as np
 from collections import Counter
 import config
+
+class MetricsCalculator:
+    """Calculates real-time network metrics"""
+    
+    def __init__(self, network):
+        self.G = network
+        self._centrality_cache = {}
+        self._community_cache = None

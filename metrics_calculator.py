375e81bfda31d051d611b7b24d4139248fd38034
natnael-eyuel-dev <natnael.eyuel.dev@gmail.com>
2026-02-01 19:57:39 +0300

    feat: add centrality metrics calculation with caching and thresholds

--- metrics_calculator.py
+++ metrics_calculator.py
@@ -33,3 +33,43 @@ class MetricsCalculator:
         if self.G.number_of_nodes() == 0:
             return 0.0
         return nx.average_clustering(self.G)
+    
+    def calculate_centrality_metrics(self, force_recalculate=False):
+        """Calculate various centrality measures"""
+        if not force_recalculate and self._centrality_cache:
+            return self._centrality_cache
+        
+        metrics = {}
+        
+        # Degree Centrality (always fast)
+        metrics['degree'] = nx.degree_centrality(self.G)
+        
+        # Betweenness Centrality (slow for large networks)
+        if config.CALCULATE_BETWEENNESS and self.G.number_of_nodes() < 1000:
+            try:
+                metrics['betweenness'] = nx.betweenness_centrality(self.G)
+            except:
+                metrics['betweenness'] = {}
+        else:
+            metrics['betweenness'] = {}
+        
+        # Closeness Centrality (only for connected graphs)
+        if nx.is_connected(self.G) and self.G.number_of_nodes() < 500:
+            try:
+                metrics['closeness'] = nx.closeness_centrality(self.G)
+            except:
+                metrics['closeness'] = {}
+        else:
+            metrics['closeness'] = {}
+        
+        # Eigenvector Centrality
+        if self.G.number_of_nodes() < 500:
+            try:
+                metrics['eigenvector'] = nx.eigenvector_centrality(self.G, max_iter=100)
+            except:
+                metrics['eigenvector'] = {}
+        else:
+            metrics['eigenvector'] = {}
+        
+        self._centrality_cache = metrics
+        return metrics

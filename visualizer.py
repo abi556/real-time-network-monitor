bdf636e60bd4b6e466da892f89ec69415f22e0b4
Yamlak Negash <164669390+yamneg96@users.noreply.github.com>
2026-02-01 19:39:52 +0300

    Update Visualizer file
    Created a class and initializer function.

--- visualizer.py
+++ visualizer.py
@@ -4,3 +4,9 @@ import plotly.graph_objects as go
 import numpy as np
 import matplotlib.pyplot as plt
 import config
+
+class NetworkVisualizer:
+    """Creates interactive network visualizations"""
+    
+    def __init__(self, network):
+        self.G = network
